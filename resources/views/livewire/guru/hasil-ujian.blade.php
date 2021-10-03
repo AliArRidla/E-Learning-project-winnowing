600eb53658752930cfe74786d75eb3a8287cbc83
nsenja <nabilasw34@gmail.com>
2021-10-03 14:16:56 +0700

    alter tabel soal essays

--- resources/views/livewire/guru/hasil-ujian.blade.php
+++ resources/views/livewire/guru/hasil-ujian.blade.php
@@ -138,7 +138,7 @@
     <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Review Ulangan</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -169,7 +169,7 @@
         </div>
         <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary" href = "{{ route('listHasilGuru', ['nav_dmid' => $nav_dmid, 'id_ul' => $id_ul]) }}">Save changes</button>
         </div>
     </div>
     </div>
